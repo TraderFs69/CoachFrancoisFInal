9d1e5214712db1b342a804ee7f195002f95aa256
TraderFs69 <fsanscartier@hotmail.com>
2026-01-16 21:58:39 -0500

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,18 +1,18 @@
 import ta
 
 def compute_indicators(df):
+
     df["EMA50"] = ta.trend.ema_indicator(df["Close"], 50)
     df["EMA200"] = ta.trend.ema_indicator(df["Close"], 200)
 
     macd = ta.trend.MACD(df["Close"], 8, 21, 9)
-    df["MACD"] = macd.macd()
-    df["MACD_signal"] = macd.macd_signal()
     df["MACD_hist"] = macd.macd_diff()
 
     df["RSI"] = ta.momentum.rsi(df["Close"], 14)
     df["ADX"] = ta.trend.adx(df["High"], df["Low"], df["Close"], 14)
 
     df["Volume_MA20"] = df["Volume"].rolling(20).mean()
+
     df["ATR"] = ta.volatility.average_true_range(
         df["High"], df["Low"], df["Close"], 14
     )
