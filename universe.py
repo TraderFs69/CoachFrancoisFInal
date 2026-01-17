ba5d1ec3aee4332b90a0450dd75ad0fbcc0d0c27
TraderFs69 <fsanscartier@hotmail.com>
2026-01-16 21:58:54 -0500

    Update universe.py

--- universe.py
+++ universe.py
@@ -2,23 +2,23 @@ import pandas as pd
 import streamlit as st
 
 def load_sp500_universe():
+
     uploaded_file = st.file_uploader(
         "📂 Téléverse le fichier sp500_constituents.xlsx",
         type=["xlsx"]
     )
 
     if uploaded_file is None:
-        st.warning("⬆️ Veuillez téléverser le fichier S&P 500.")
+        st.warning("Veuillez téléverser le fichier S&P 500.")
         return []
 
     df = pd.read_excel(uploaded_file)
 
-    # adapte ici si le nom de colonne diffère
     if "Symbol" not in df.columns:
-        st.error("❌ Colonne 'Symbol' introuvable dans le fichier.")
+        st.error("La colonne 'Symbol' est introuvable.")
         return []
 
-    tickers = (
+    return (
         df["Symbol"]
         .dropna()
         .astype(str)
@@ -26,5 +26,3 @@ def load_sp500_universe():
         .unique()
         .tolist()
     )
-
-    return tickers
